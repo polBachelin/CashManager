f5ce6cf36509203210981d495e7d8c45e3d4358b
EvanSabre <evan.sabre@epitech.eu>
2022-11-21 17:06:12 +0100

    feat(): QR code screen carré

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cash_manager/pages/QR_code.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'pages/select_server.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             checkerboardOffscreenLayers: false,
             routes: {
-          '/': (context) => const ServerPage(),
+          '/': (context) => const QRcodePage(),
           // '/login': (context) => const LoginScreen(),
           // '/register': (context) => const RegisterScreen(),
           // '/buy': (context) => const BuyScreen(),
--- flutter_app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ flutter_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/mathis/FLUTTER/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/mathis/Documents/COURS EPITECH/MSC1/Cash Management/T-DEV-700-REN_3/flutter_app"
+export "FLUTTER_ROOT=/home/enoxes/flutter"
+export "FLUTTER_APPLICATION_PATH=/mnt/d/4e_année/T-DEV-700-REN_3/flutter_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
