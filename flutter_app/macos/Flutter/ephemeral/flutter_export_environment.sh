c0da7edb3121956b6ea01e7d4fb4e5d3eb99c956
mathis <mathis.serveau@epitech.eu>
2022-11-21 15:37:16 +0100

    merge local work

---
+++ flutter_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/mathis/FLUTTER/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/mathis/Documents/COURS EPITECH/MSC1/Cash Management/T-DEV-700-REN_3/flutter_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
