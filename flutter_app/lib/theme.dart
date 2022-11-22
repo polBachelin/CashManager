f8ae39ef5899943216ac10694aa754d343382fbe
mathis <mathis.serveau@epitech.eu>
2022-11-22 14:55:45 +0100

    add theme color

--- flutter_app/lib/theme.dart
+++ flutter_app/lib/theme.dart
@@ -1,40 +1,22 @@
-library color_theme;
+// ignore_for_file: constant_identifier_names
+
+library color_theme; // ignore_for_file: unused_import
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cash_manager/theme.dart' as theme;
 
-const Color primaryColor = Color.fromARGB(255, 211, 109, 42);
-const Color primaryLightColor = Color.fromARGB(255, 214, 86, 0);
-const Color reset = Color.fromARGB(255, 197, 0, 0);
-const Color labelColor = Color(0xffc5c5c5);
-
-const Color white = Color(0xffffffff);
-const Color lightText = Color(0xffffffff);
-
-const Color githubBlack = Color(0xff171515);
-const Color background = Color.fromARGB(255, 0, 0, 0);
-
-const Color disable = Color.fromARGB(255, 156, 156, 156);
-const Color validate = Color.fromARGB(255, 224, 167, 8);
+const Color blueColor = Color(0xff21579C);
+const Color ColorBackground = Color.fromARGB(255, 244, 251, 249);
+const Color ColorLogo = Color.fromARGB(255, 51, 119, 165);
+const Color ColorText = Color.fromARGB(255, 50, 163, 138);
+const Color ColorButton = Color.fromARGB(255, 41, 98, 133);
+const Color ColorTextButton = Color.fromARGB(255, 255, 255, 255);
 
 TextStyle titleStyle = GoogleFonts.poppins(
-    color: primaryColor, fontSize: 25, fontWeight: FontWeight.w600);
+    color: ColorText, fontSize: 25, fontWeight: FontWeight.w600);
 TextStyle buttonTextStyle = GoogleFonts.poppins(
   color: const Color.fromARGB(255, 255, 255, 255),
   fontSize: 16,
   fontWeight: FontWeight.w500,
-);
-
-InputDecoration decorationInput = InputDecoration(
-  enabledBorder: OutlineInputBorder(
-    borderSide: const BorderSide(color: theme.primaryColor, width: 2),
-    borderRadius: BorderRadius.circular(20),
-  ),
-  border: OutlineInputBorder(
-    borderSide: const BorderSide(color: theme.primaryColor, width: 2),
-    borderRadius: BorderRadius.circular(20),
-  ),
-  filled: true,
-  fillColor: Colors.white,
 );
